bc7bdf40dc3e6278b10a63201a4742fefbbee178
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-10 12:27:10 +0100

    add db connection

---
+++ db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const	mongoose = require('mongoose');
+const	url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false);
+
+const	connectDB = async () =>
+{
+	try 
+	{
+		await mongoose.connect(url);
+		console.log('connected to MongoDB');
+	} 
+	catch (error) 
+	{
+		console.log(`Error connecting to MongoDB: ${error}`);
+		process.exit(1);
+	}
+}
+
+module.exports = connectDB;
