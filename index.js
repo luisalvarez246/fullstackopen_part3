8eb78fc97a7ac4dc17120e6c2a508c6d8f55155b
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-11-14 18:12:45 +0100

    refactor getAll, getById, and save operations
    index.js is now configured as a controller

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const	PORT = process.env.PORT || 3002;
 const	personsUrl = '/api/persons';
 const	infoUrl = '/info';
 const	url = `http://localhost:${PORT}${personsUrl}`;
-// let		persons;
 const	db = require('./db');
-const	Person = require('./models/Person');
+const	personsService = require('./services/PersonsService');
 
 db.connectDB();
 app.use(cors());
@@ -23,30 +22,6 @@ morgan.token('data', (request) =>
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
-// persons = 
-// [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
-
 const formatTime = () =>
 {
 	const now = new Date();
@@ -55,77 +30,14 @@ const formatTime = () =>
 	return (formattedTimestamp);
 }
 
-const generateId = () =>
+app.get(personsUrl, async (request, response) => 
 {
-	const personsMaxId = Math.max(...persons.map(person => person.id));
-	const id = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - personsMaxId + 1)) + personsMaxId;
-
-	return (id);
-};
-
-const parseRequest = async (body) =>
-{
-	let		error;
-	const	isInDB = await Person.find({name: body.name});
-
-	if (!body.name && !body.number)
-	{
-		error = 'name AND/OR number must not be empty';
-	}
-	else if (!body.name)
-	{
-		error ='name must not be empty';
-	}
-	else if (!body.number)
-	{
-		error ='number must not be empty';
-	}
-	else if (isInDB.length > 0)
-	{
-		error = 'name must be unique';
-	}
-	else
-	{
-		error = null;
-	}
-	return (error);
-
-};
-
-app.get(personsUrl, async (request, response) =>
-{
-	try
-	{
-		const	result = await Person.find({});
-
-		response.json(result);
-	}
-	catch(error)
-	{
-		console.log(`Could not fetch persons: ${error}`);
-	}
+	await personsService.getAllPersons(request, response);
 })
 
-app.get(`${personsUrl}/:id`, async (request, response) =>
+app.get(`${personsUrl}/:id`, async (request, response) => 
 {
-	const	id = request.params.id;
-	try
-	{
-		const	person = await Person.findById(id);
-		if (person)
-		{
-			response.json(person);
-		}
-		else
-		{
-			response.status(404);
-			response.send(`person with id ${id} not found`);
-		}
-	}
-	catch(error)
-	{
-		console.log(`Could not fetch person: ${error}`);
-	}
+	await personsService.getPersonById(request, response);
 })
 
 app.delete(`${personsUrl}/:id`, (request, response) =>
@@ -136,32 +48,10 @@ app.delete(`${personsUrl}/:id`, (request, response) =>
 	response.status(204).end();
 })
 
-app.post(personsUrl, async (request, response) =>
+app.post(personsUrl, async (request, response) => 
 {
-	const	body = request.body;
-	const	error = await parseRequest(body);	
-	const	person = new Person(
-	{
-		name: body.name,
-		number: body.number
-	})
-
-	if (error)
-	{
-		console.log('enters here');
-		return (response.status(400).json({error: error}));
-	}
-	try
-	{
-		const savedNote = await person.save();
-		response.json(savedNote);
-	}
-	catch(error)
-	{
-		console.log(`Error saving note: ${error}`);
-	}
+	await personsService.savePerson(request, response);
 })
-
 
 app.get(infoUrl, (request, response) =>
 {
